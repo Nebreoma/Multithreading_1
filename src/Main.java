129f2c2c4227162811dbd94d145aa518b0737614
Nebreoma <Nebreoma12@gmail.com>
2024-07-03 15:38:43 +0300

    add code on branch max

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,68 @@
 import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static java.lang.String.join;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws ExecutionException {
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
 
         long startTs = System.currentTimeMillis(); // start time
-        List<Thread> threads = new ArrayList<Thread>();
+        List<Future<Map<String, Integer>>> futures = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(5);
 
         for (String text : texts) {
-            Thread thread = new Thread() {
-                public void run() {
-                    int maxSize = 0;
-                    for (int i = 0; i < text.length(); i++) {
-                        for (int j = 0; j < text.length(); j++) {
-                            if (i >= j) {
-                                continue;
-                            }
-                            boolean bFound = false;
-                            for (int k = i; k < j; k++) {
-                                if (text.charAt(k) == 'b') {
-                                    bFound = true;
-                                    break;
-                                }
-                            }
-                            if (!bFound && maxSize < j - i) {
-                                maxSize = j - i;
+
+            Future<Map<String, Integer>> future = executor.submit(() -> {
+
+                int maxSize = 0;
+                Map<String, Integer> map = new HashMap<>();
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
                             }
                         }
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
+                        }
                     }
-                    System.out.println(text.substring(0, 100) + " -> " + maxSize);
-                }//конец run
-            };//конец лямбды thread
-            threads.add(thread);
-            thread.start();
+                }
+                map.put(text, maxSize);
+                return map;
+            });//конец лямбды future
+
+            futures.add(future);
         }
 
-        for (Thread t : threads) {
-            t.join();
+        executor.shutdown();
+
+        for (Future<Map<String, Integer>> f : futures) {
+            try {
+                Map<String, Integer> map = f.get();
+                String text = map.keySet().toString();
+                System.out.println(text.substring(0, 100) + " -> " + map.values());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
 
         long endTs = System.currentTimeMillis(); // end time
+
         System.out.println("Time: " + (endTs - startTs) + "ms");
     }//конец main
 
@@ -58,4 +77,5 @@ public class Main {
 }
 
 //первоначальный код = 59 014 ms
-//Нынешний код = 8 103 ms
+//Код в main = 8 103 ms
+//Код в max = 8 920 ms
